20d9f3e5be4b1b840781245d4f04c58403fd58d7
yang372193411 <yang372193411@163.com>
2018-07-29 11:06:56 +0800

    spring-core工程：自动Mybatis代码生成注释功能

--- src/main/java/com/ityang/basic/util/MyCommentGenerator.java
+++ src/main/java/com/ityang/basic/util/MyCommentGenerator.java
@@ -4,12 +4,14 @@ import org.mybatis.generator.api.CommentGenerator;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.TextElement;
 import org.mybatis.generator.api.dom.xml.XmlElement;
 import org.mybatis.generator.config.MergeConstants;
 import org.mybatis.generator.config.PropertyRegistry;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
 import static org.mybatis.generator.internal.util.StringUtility.isTrue;
@@ -58,18 +60,25 @@ public class MyCommentGenerator implements CommentGenerator {
      */
     @Override
     public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
-        if (suppressAllComments) {
-            return;
-        }
-
-        StringBuilder sb = new StringBuilder();
+//        if (suppressAllComments) {
+//            return;
+//        }
+//        StringBuilder sb = new StringBuilder();
+//        innerClass.addJavaDocLine("/**");
+//        sb.append(" * ");
+//        sb.append(introspectedTable.getFullyQualifiedTable());
+//        sb.append(" ");
+//        sb.append(getDateString());
+//        innerClass.addJavaDocLine(sb.toString().replace("\n", " "));
+//        innerClass.addJavaDocLine(" */");
         innerClass.addJavaDocLine("/**");
-        sb.append(" * ");
-        sb.append(introspectedTable.getFullyQualifiedTable());
-        sb.append(" ");
-        sb.append(getDateString());
-        innerClass.addJavaDocLine(sb.toString().replace("\n", " "));
+        innerClass.addJavaDocLine(" * @Title "+introspectedTable.getFullyQualifiedTable()+"表的实体类");
+        innerClass.addJavaDocLine(" * @Description "+introspectedTable.getFullyQualifiedTable());//.getRemarks());
+        innerClass.addJavaDocLine(" * @version 1.0");
+        innerClass.addJavaDocLine(" * @Author yang");
+        innerClass.addJavaDocLine(" * @Date " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         innerClass.addJavaDocLine(" */");
+
     }
 
     /**
@@ -77,21 +86,29 @@ public class MyCommentGenerator implements CommentGenerator {
      */
     @Override
     public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
-        if (suppressAllComments) {
-            return;
-        }
-        StringBuilder sb = new StringBuilder();
+//        if (suppressAllComments) {
+//            return;
+//        }
+//        StringBuilder sb = new StringBuilder();
+//        innerClass.addJavaDocLine("/**");
+//        sb.append(" * ");
+//        sb.append(" * ");
+//        sb.append(introspectedTable.getFullyQualifiedTable());
+//        innerClass.addJavaDocLine(sb.toString().replace("\n", " "));
+//        sb.setLength(0);
+//        sb.append(" * @author ");
+//        sb.append(systemPro.getProperty("user.name"));
+//        sb.append(" ");
+//        sb.append(currentDateStr);
+//        innerClass.addJavaDocLine(" */");
         innerClass.addJavaDocLine("/**");
-        sb.append(" * ");
-        sb.append(" * ");
-        sb.append(introspectedTable.getFullyQualifiedTable());
-        innerClass.addJavaDocLine(sb.toString().replace("\n", " "));
-        sb.setLength(0);
-        sb.append(" * @author ");
-        sb.append(systemPro.getProperty("user.name"));
-        sb.append(" ");
-        sb.append(currentDateStr);
+        innerClass.addJavaDocLine(" * @Title "+introspectedTable.getFullyQualifiedTable()+"表的实体类");
+        innerClass.addJavaDocLine(" * @Description "+introspectedTable.getFullyQualifiedTable().getIntrospectedTableName());//.getRemarks());
+        innerClass.addJavaDocLine(" * @version 1.0");
+        innerClass.addJavaDocLine(" * @Author yang");
+        innerClass.addJavaDocLine(" * @Date " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         innerClass.addJavaDocLine(" */");
+
     }
 
 
@@ -100,7 +117,11 @@ public class MyCommentGenerator implements CommentGenerator {
      */
     @Override
     public void addComment(XmlElement xmlElement) {
-
+//        xmlElement.addElement(new TextElement("<!--"));
+//        StringBuilder sb = new StringBuilder();
+//        sb.append(MergeConstants.NEW_ELEMENT_TAG);
+//        xmlElement.addElement(new TextElement(sb.toString()));
+//        xmlElement.addElement(new TextElement("-->"));
     }
 
     /**
@@ -203,11 +224,17 @@ public class MyCommentGenerator implements CommentGenerator {
         if (suppressAllComments) {
             return;
         }
-        StringBuilder sb = new StringBuilder();
+        // 添加字段注释
+        StringBuffer sb = new StringBuffer();
         field.addJavaDocLine("/**");
-        sb.append(" * ");
-        sb.append(introspectedColumn.getRemarks());
-        field.addJavaDocLine(sb.toString().replace("\n", " "));
+        if (introspectedColumn.getRemarks() != null)
+            field.addJavaDocLine(" * " + introspectedColumn.getRemarks());
+        sb.append(" * 表字段 : ");
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append('.');
+        sb.append(introspectedColumn.getActualColumnName());
+        field.addJavaDocLine(sb.toString());
+        // addJavadocTag(field, false);
         field.addJavaDocLine(" */");
     }
 
@@ -216,11 +243,82 @@ public class MyCommentGenerator implements CommentGenerator {
      */
     @Override
     public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
-        if (suppressAllComments) {
-            return;
+        StringBuilder sb = new StringBuilder();
+        method.addJavaDocLine("/**"); //$NON-NLS-1$
+        sb.append(" * ");
+        if (method.isConstructor()) {
+            sb.append(" 构造查询条件");
         }
-        method.addJavaDocLine("/**");
-        addJavadocTag(method, false);
+        String method_name = method.getName();
+        if ("setOrderByClause".equals(method_name)) {
+            sb.append(" 设置排序字段");
+        } else if ("setDistinct".equals(method_name)) {
+            sb.append(" 设置过滤重复数据");
+        } else if ("getOredCriteria".equals(method_name)) {
+            sb.append(" 获取当前的查询条件实例");
+        } else if ("isDistinct".equals(method_name)) {
+            sb.append(" 是否过滤重复数据");
+        } else if ("getOrderByClause".equals(method_name)) {
+            sb.append(" 获取排序字段");
+        } else if ("createCriteria".equals(method_name)) {
+            sb.append(" 创建一个查询条件");
+        } else if ("createCriteriaInternal".equals(method_name)) {
+            sb.append(" 内部构建查询条件对象");
+        } else if ("clear".equals(method_name)) {
+            sb.append(" 清除查询条件");
+        } else if ("countByExample".equals(method_name)) {
+            sb.append(" 根据指定的条件获取数据库记录数");
+        } else if ("deleteByExample".equals(method_name)) {
+            sb.append(" 根据指定的条件删除数据库符合条件的记录");
+        } else if ("deleteByPrimaryKey".equals(method_name)) {
+            sb.append(" 根据主键删除数据库的记录");
+        } else if ("insert".equals(method_name)) {
+            sb.append(" 新写入数据库记录");
+        } else if ("insertSelective".equals(method_name)) {
+            sb.append(" 动态字段,写入数据库记录");
+        } else if ("selectByExample".equals(method_name)) {
+            sb.append(" 根据指定的条件查询符合条件的数据库记录");
+        } else if ("selectByPrimaryKey".equals(method_name)) {
+            sb.append(" 根据指定主键获取一条数据库记录");
+        } else if ("updateByExampleSelective".equals(method_name)) {
+            sb.append(" 动态根据指定的条件来更新符合条件的数据库记录");
+        } else if ("updateByExample".equals(method_name)) {
+            sb.append(" 根据指定的条件来更新符合条件的数据库记录");
+        } else if ("updateByPrimaryKeySelective".equals(method_name)) {
+            sb.append(" 动态字段,根据主键来更新符合条件的数据库记录");
+        } else if ("updateByPrimaryKey".equals(method_name)) {
+            sb.append(" 根据主键来更新符合条件的数据库记录");
+        }
+        sb.append(",");
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        method.addJavaDocLine(sb.toString());
+
+        final List<Parameter> parameterList = method.getParameters();
+        if (!parameterList.isEmpty()) {
+            method.addJavaDocLine(" *");
+            if ("or".equals(method_name)) {
+                sb.append(" 增加或者的查询条件,用于构建或者查询");
+            }
+        } else {
+            if ("or".equals(method_name)) {
+                sb.append(" 创建一个新的或者查询条件");
+            }
+        }
+        String paramterName;
+        for (Parameter parameter : parameterList) {
+            sb.setLength(0);
+            sb.append(" * @param "); //$NON-NLS-1$
+            paramterName = parameter.getName();
+            sb.append(paramterName);
+            if ("orderByClause".equals(paramterName)) {
+                sb.append(" 排序字段"); //$NON-NLS-1$
+            } else if ("distinct".equals(paramterName)) {
+                sb.append(" 是否过滤重复数据");
+            } else if ("criteria".equals(paramterName)) {
+                sb.append(" 过滤条件实例");
+            }
+            method.addJavaDocLine(sb.toString());
+        }
         method.addJavaDocLine(" */");
     }
 
@@ -230,21 +328,24 @@ public class MyCommentGenerator implements CommentGenerator {
      */
     @Override
     public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
-        if (suppressAllComments) {
-            return;
-        }
-        method.addJavaDocLine("/**");
         StringBuilder sb = new StringBuilder();
-        sb.append(" * ");
-        sb.append(introspectedColumn.getRemarks());
-        method.addJavaDocLine(sb.toString().replace("\n", " "));
-        sb.setLength(0);
-        sb.append(" * @return ");
+        method.addJavaDocLine("/**"); //$NON-NLS-1$
+        sb.append(" * 获取 "); //$NON-NLS-1$
+        sb.append(introspectedColumn.getRemarks()).append(" 字段:");
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append('.');
         sb.append(introspectedColumn.getActualColumnName());
-        sb.append(" ");
+        method.addJavaDocLine(sb.toString());
+        method.addJavaDocLine(" *"); //$NON-NLS-1$
+        sb.setLength(0);
+        sb.append(" * @return "); //$NON-NLS-1$
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append('.');
+        sb.append(introspectedColumn.getActualColumnName());
+        sb.append(", ");
         sb.append(introspectedColumn.getRemarks());
-        method.addJavaDocLine(sb.toString().replace("\n", " "));
-        method.addJavaDocLine(" */");
+        method.addJavaDocLine(sb.toString());
+        method.addJavaDocLine(" */"); //$NON-NLS-1$
     }
 
     /**
@@ -256,7 +357,7 @@ public class MyCommentGenerator implements CommentGenerator {
         compilationUnit.addFileCommentLine("/*");
         compilationUnit.addFileCommentLine("*");
         compilationUnit.addFileCommentLine("* " + compilationUnit.getType().getShortName() + ".java");
-        compilationUnit.addFileCommentLine("* Copyright(C) 2017-2020 fendo公司");
+        compilationUnit.addFileCommentLine("* Copyright(C) 2017-2020 yang");
         compilationUnit.addFileCommentLine("* @date " + sdf.format(new Date()) + "");
         compilationUnit.addFileCommentLine("*/");
     }
@@ -283,22 +384,27 @@ public class MyCommentGenerator implements CommentGenerator {
      */
     @Override
     public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
-        if (suppressAllComments) {
-            return;
-        }
-        method.addJavaDocLine("/**");
         StringBuilder sb = new StringBuilder();
-        sb.append(" * ");
-        sb.append(introspectedColumn.getRemarks());
-        method.addJavaDocLine(sb.toString().replace("\n", " "));
+        method.addJavaDocLine("/**"); //$NON-NLS-1$
+        sb.append(" * 设置 ");  //$NON-NLS-1$
+        sb.append(introspectedColumn.getRemarks()).append(" 字段:");
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append('.');
+        sb.append(introspectedColumn.getActualColumnName());
+        method.addJavaDocLine(sb.toString());
+        method.addJavaDocLine(" *"); //$NON-NLS-1$
         Parameter parm = method.getParameters().get(0);
         sb.setLength(0);
-        sb.append(" * @param ");
+        sb.append(" * @param "); //$NON-NLS-1$
         sb.append(parm.getName());
-        sb.append(" ");
+        sb.append(" the value for "); //$NON-NLS-1$
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append('.');
+        sb.append(introspectedColumn.getActualColumnName());
+        sb.append(", ");
         sb.append(introspectedColumn.getRemarks());
-        method.addJavaDocLine(sb.toString().replace("\n", " "));
-        method.addJavaDocLine(" */");
+        method.addJavaDocLine(sb.toString());
+        method.addJavaDocLine(" */"); //$NON-NLS-1$
     }
 
 }
